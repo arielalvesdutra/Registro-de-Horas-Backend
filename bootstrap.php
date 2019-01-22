c187c19f513b3ba650358773613bb1d49c7e5f8a
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-22 16:19:49 -0200

    Adiciona método printrx no bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,24 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+
+/**
+ * Método de Depuração
+ */
+function printrx()
+{
+    $arguments = func_get_args();
+    echo '<pre>';
+    foreach($arguments as &$argument){
+        if (is_object($argument) || is_array($argument)) {
+            print_r($argument);
+        } elseif (empty($argument) || is_resource($argument)) {
+            var_dump($argument);
+        } else {
+            echo (string)$argument;
+        }
+    }
+    echo '</pre>';
+    die();
+}
