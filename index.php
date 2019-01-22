91e31db57395536717546b4d297f0524a7db2d9c
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-22 16:21:56 -0200

    Adiciona rotas do TimeRecorder

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require 'bootstrap.php';
 
+use App\Controllers\Controller;
+use App\Controllers\TimeRecorder;
 use Slim\App;
 
 $app = new App([
@@ -12,8 +14,26 @@ $app = new App([
     ]
 ]);
 
+/**
+ * Raiz
+ */
 $app->get('/', function(){
-   echo "Iniciando a aplciação Backend de Registro de horas...";
+   echo "Iniciando a aplicação Backend de Registro de horas...";
 });
+
+/**
+ * Controller
+ */
+$app->get('/test', Controller::class. ':test');
+
+
+/**
+ * Time Recorder
+ */
+$app->delete('/deleteRecord/{id}', TimeRecorder::class . ':deleteRecord');
+$app->get('/getRecords', TimeRecorder::class . ':getRecords');
+$app->post('/addRecord', TimeRecorder::class . ':addRecord');
+$app->put('/updateRecord/{id}', TimeRecorder::class . ':updateRecord');
+
 
 $app->run();
