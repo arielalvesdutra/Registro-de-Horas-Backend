79215f69c9208ed1af907cf9edceb18508ed898e
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-22 16:20:51 -0200

    Adiciona controller Controller

---
+++ App/Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Controller {
+
+    public function test()
+    {
+        echo "It works!";
+    }
+}
