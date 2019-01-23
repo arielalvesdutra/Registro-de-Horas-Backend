e64f0a31b705454e0a69098c86e0f40a71d51d4f
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-23 11:42:16 -0200

    Adiciona método de depuração que não encerra a execução do código

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,27 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-
 /**
  * Método de Depuração
+ */
+function printr()
+{
+    $arguments = func_get_args();
+    echo '<pre>';
+    foreach($arguments as &$argument){
+        if (is_object($argument) || is_array($argument)) {
+            print_r($argument);
+        } elseif (empty($argument) || is_resource($argument)) {
+            var_dump($argument);
+        } else {
+            echo (string)$argument;
+        }
+    }
+    echo '</pre>';
+}
+
+/**
+ * Método de Depuração que encerra execução do código
  */
 function printrx()
 {
