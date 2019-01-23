28c7874c21ebf62f29e410e91ce7c969f75ec985
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-23 19:04:52 -0200

    Adiciona atributos e métodos de acesso para entidade de registro

--- App/Entities/TimeRecord.php
+++ App/Entities/TimeRecord.php
@@ -5,4 +5,51 @@ namespace App\Entities;
 class TimeRecord extends Entity
 {
 
+    protected $title;
+
+    protected $initTime;
+
+    protected $endTime;
+
+    protected $duration;
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setInitTime(string $initTime): void
+    {
+        $this->initTime = $initTime;
+    }
+
+    public function getInitTime(): string
+    {
+        return $this->initTime;
+    }
+
+    public function setEndTime(string $endTime): void
+    {
+        $this->endTime = $endTime;
+    }
+
+    public function getEndTime(): string
+    {
+        return $this->endTime;
+    }
+
+    public function setDuration(string $duration): void
+    {
+        $this->duration = $duration;
+    }
+
+    public function getDuration(): string
+    {
+        return $this->duration;
+    }
 }
