5d135947afcb2f01f55c9b568f290f7c36dc7165
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-24 11:08:47 -0200

    Implementa método para na repositorie para adicionar registro de tempo

--- App/Repositories/TimeRecorder.php
+++ App/Repositories/TimeRecorder.php
@@ -2,11 +2,24 @@
 
 namespace App\Repositories;
 
+use App\Factories;
+use App\Models;
+use App\Services\TimeRecorderService;
+
 class TimeRecorder extends Repository
 {
-
-    public function addTimeRecord($parameters = [])
+    
+    public static function addTimeRecord($parameters = [])
     {
 
+        $timeRecord = Factories\TimeRecord::create($parameters);
+
+        $timeRecorderService = new TimeRecorderService();
+
+        $timeRecord->setDuration(
+            $timeRecorderService->calculateTimeDuration($timeRecord)
+        );
+
+        Models\TimeRecord::save($timeRecord);
     }
 }
