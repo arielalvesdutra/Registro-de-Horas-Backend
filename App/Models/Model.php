a097c782fad5ab67094a550916b4bfd580292e3d
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-24 16:29:33 -0200

    Adiciona na Model método para retornar todos os registros do banco

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -8,4 +8,14 @@ abstract class Model {
 
     protected $table;
 
+    public static function all()
+    {
+        $object = new static();
+        $pdo = Database::connect();
+
+        $query = $pdo->prepare("SELECT * FROM ". $object->table);
+        $query->execute();
+
+        return $query->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
