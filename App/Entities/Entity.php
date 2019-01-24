eff0e56f4adf30dbe13d0c3010fc78befdb60551
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-24 16:28:05 -0200

    Adiciona id no construtor de Entidade

--- App/Entities/Entity.php
+++ App/Entities/Entity.php
@@ -6,6 +6,13 @@ class Entity
 {
     private $id;
 
+    public function __construct($parameters = [])
+    {
+        if ($parameters['id']) {
+            $this->setId((int) $parameters['id']);
+        }
+    }
+
     protected function setId(int $id)
     {
         $this->id = $id;
