0beec69229175d5f937c5505f2bed85a6202eede
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-24 11:06:22 -0200

    Adiciona método para criar um registro de tempo

--- App/Factories/TimeRecord.php
+++ App/Factories/TimeRecord.php
@@ -2,7 +2,22 @@
 
 namespace App\Factories;
 
+use App\Entities;
+
 class TimeRecord extends Factory
 {
 
+    public static function create($parameters = [])
+    {
+        $timeRecord = (new Entities\TimeRecord())
+            ->setTitle($parameters['title'])
+            ->setInitTime($parameters['initDate'])
+            ->setEndTime($parameters['endDate']);
+
+        if ($parameters['duration']) {
+            $timeRecord->setDuration($parameters['duration']);
+        }
+
+        return $timeRecord;
+    }
 }
