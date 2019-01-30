7c0d5bb824e813eb8bf4a49c704b626f5200ad39
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-30 16:51:38 -0200

    Adiciona Access-Controll Allow-Origin, Allow-Headers e Allow-Methods

--- index.php
+++ index.php
@@ -14,6 +14,14 @@ $app = new App([
     ]
 ]);
 
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD');
+});
+
 /**
  * Raiz
  */
@@ -37,6 +45,5 @@ $app->delete('/deleteRecord/{id}', TimeRecorder::class . ':deleteRecord');
 $app->get('/getRecords', TimeRecorder::class . ':getRecords');
 $app->post('/addRecord', TimeRecorder::class . ':addRecord');
 $app->put('/updateRecord/{id}', TimeRecorder::class . ':updateRecord');
-
 
 $app->run();
