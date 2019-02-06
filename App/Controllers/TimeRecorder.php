e914f2a5b44e414c80b2e57b0604f0038c608f33
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-02-06 14:00:22 -0200

    Modifica a controller TimeRecorder para usar uma implementação semelhante a injeção de dependência, Utiliza métodos da dependência e não métodos estáticos

--- App/Controllers/TimeRecorder.php
+++ App/Controllers/TimeRecorder.php
@@ -2,56 +2,74 @@
 
 namespace App\Controllers;
 
+use App\Models;
 use App\Repositories;
+use App\Services;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class TimeRecorder extends Controller {
 
-    public function addRecord(ServerRequestInterface $request)
+    public function __construct()
     {
-        $parameters = $request->getParsedBody();
-
-        Repositories\TimeRecorder::addTimeRecord($parameters);
+        $this->setRepository(new Repositories\TimeRecorder(
+            new Models\TimeRecord(),
+            new Services\TimeRecorderService()
+        ));
     }
 
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    public function addRecord(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        try {
+
+            $parameters = $request->getParsedBody();
+
+            $this->repository->addTimeRecord($parameters);
+
+            return $response->withStatus(200);
+        } catch (\Exception $exception) {
+            return $response->withStatus(500);
+        }
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     */
     public function deleteRecord(ServerRequestInterface $request)
     {
         $id = $request->getAttribute('id');
 
-        Repositories\TimeRecorder::deleteTimeRecord($id);
+        $this->repository->deleteTimeRecord($id);
     }
 
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     *
+     * @return JSON
+     */
     public function getRecords(ServerRequestInterface $request, ResponseInterface $response)
     {
-        $records  = Repositories\TimeRecorder::getTimeRecords();
+        $queryParameters = $request->getQueryParams();
+
+        $records = $this->repository->getTimeRecords($queryParameters);
 
         return $response->withJson($records);
     }
 
-    public function getRecordsByInitDate(ServerRequestInterface $request, ResponseInterface $response)
-    {
-        $date = $request->getAttribute('date');
-
-        $records = Repositories\TimeRecorder::getTimeRecordsByInitDate($date);
-
-        return $response->withJson($records);
-    }
-
-    public function getRecordsByFilters(ServerRequestInterface $request, ResponseInterface $response)
-    {
-        $queryParams = $request->getQueryParams();
-
-        $records = Repositories\TimeRecorder::getTimeRecordsByFilters($queryParams);
-
-        return $response->withJson($records);
-    }
-
-
+    /**
+     * @param ServerRequestInterface $request
+     */
     public function updateRecord(ServerRequestInterface $request)
     {
         $parameters = $request->getParsedBody();
 
-        Repositories\TimeRecorder::updateTimeRecord($parameters);
+        $this->repository->updateTimeRecord($parameters);
     }
 }
