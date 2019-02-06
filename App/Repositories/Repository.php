916dec1ff38f77fe1935791340ff0d79763d2603
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-02-06 16:05:06 -0200

    Adiciona documentação

--- App/Repositories/Repository.php
+++ App/Repositories/Repository.php
@@ -8,8 +8,18 @@ use App\Services\Service;
 abstract class Repository
 {
 
+    /**
+     * Dependência de uma model
+     *
+     * @var Model
+     */
     protected $model;
 
+    /**
+     * Dependência de um service
+     *
+     * @var Service
+     */
     protected $service;
 
     public function __construct(Model $model, Service $service)
