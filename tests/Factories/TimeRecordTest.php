5fc23f3beb3c538985e1a65393217e077461ed8f
arielalvesdutra <alves.lsn@gmail.com>
2019-02-16 20:16:19 -0200

    Atualiza testes unitários da App\Factories\TimeRecord

--- tests/Factories/TimeRecordTest.php
+++ tests/Factories/TimeRecordTest.php
@@ -4,6 +4,12 @@ namespace App\Factories;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Testes unitários da fábrica de TimeRecord.
+ *
+ * Class TimeRecordTest
+ * @package App\Factories
+ */
 class TimeRecordTest extends TestCase
 {
     public function assertPostConditions(): void
@@ -14,12 +20,28 @@ class TimeRecordTest extends TestCase
         );
     }
 
+    public function testInitDateTimeGreaterThanEndDateTimeShouldThrowException()
+    {
+        $this->expectException(\Exception::class);
+
+        $parameters = [
+            "title" => "Registro de tempo",
+            "initDateTime" => "2019/01/01 12:00:00",
+            "endDateTime" => "2019/01/01 10:00:00"
+        ];
+
+        TimeRecord::create($parameters);
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function testShouldCreateATimeRecordEntityWithoutId()
     {
         $parameters = [
             "title" => "Registro de tempo",
-            "initDate" => "2019/01/01 10:00:00",
-            "endDate" =>  "2019/01/01 12:00:00"
+            "initDateTime" => "2019/01/01 10:00:00",
+            "endDateTime" =>  "2019/01/01 12:00:00"
         ];
 
         $timeRecord = TimeRecord::create($parameters);
@@ -27,13 +49,16 @@ class TimeRecordTest extends TestCase
         $this->assertInstanceOf('App\Entities\TimeRecord', $timeRecord, 'Returned object should be App\Entities\TimeRecord' );
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testShouldCreateATimeRecordEntityWithId()
     {
         $parameters = [
             "id" => 1,
             "title" => "Registro de tempo",
-            "initDate" => "2019/01/01 10:00:00",
-            "endDate" =>  "2019/01/01 12:00:00"
+            "initDateTime" => "2019/01/01 10:00:00",
+            "endDateTime" =>  "2019/01/01 12:00:00"
         ];
 
         $timeRecord = TimeRecord::create($parameters);
@@ -41,18 +66,67 @@ class TimeRecordTest extends TestCase
         $this->assertInstanceOf('App\Entities\TimeRecord', $timeRecord, 'Returned object should be App\Entities\TimeRecord' );
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testShouldCreateATimeRecordEntityWithIdAndDuration()
     {
         $parameters = [
             "id" => 1,
             "title" => "Registro de tempo",
-            "initDate" => "2019/01/01 10:00:00",
-            "endDate" =>  "2019/01/01 12:00:00",
-            "duration" => "2:00:00"
+            "initDateTime" => "2019/01/01 10:00:00",
+            "endDateTime" =>  "2019/01/01 12:00:00",
+            "duration" => [
+                "hours" => '2',
+                "minutes" => '15',
+                "seconds" => '10'
+            ]
         ];
 
         $timeRecord = TimeRecord::create($parameters);
 
         $this->assertInstanceOf('App\Entities\TimeRecord', $timeRecord, 'Returned object should be App\Entities\TimeRecord' );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testWithoutInitDateTimeParameterShouldThrowException()
+    {
+        $this->expectException(\Exception::class);
+        $parameters = [
+            "title" => "Registro de tempo",
+            "endDateTime" =>  "2019/01/01 12:00:00"
+        ];
+
+        TimeRecord::create($parameters);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testWithoutEndDateTimeParameterShouldThrowException()
+    {
+        $this->expectException(\Exception::class);
+        $parameters = [
+            "title" => "Registro de tempo",
+            "initDateTime" => "2019/01/01 10:00:00"
+        ];
+
+        TimeRecord::create($parameters);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testWithoutTitleParameterShouldThrowException()
+    {
+        $this->expectException(\Exception::class);
+        $parameters = [
+            "initDateTime" => "2019/01/01 10:00:00",
+            "endDateTime" =>  "2019/01/01 12:00:00"
+        ];
+
+        TimeRecord::create($parameters);
     }
 }
