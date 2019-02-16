dd654c497e68bec2a0b87e72411ebc0717a14a92
arielalvesdutra <alves.lsn@gmail.com>
2019-02-16 20:17:02 -0200

    Adiciona testes unitários do App\ValueObjects\Duration

---
+++ tests/ValueObjects/DurationTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\ValueObjects;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Testes unitários do objeto de valor Duration.
+ *
+ * Class DurationTest
+ * @package App\ValueObjects
+ */
+class DurationTest extends TestCase
+{
+    const HOUR_REGULAR_EXPRESSION = "/([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})/";
+
+    public function testSetDurationWithInvalidHoursDataShouldThrowAnException()
+    {
+        $this->expectException(Exception::class);
+
+        $hours = 'invalid hours';
+        $minutes = '25';
+        $seconds = '10';
+
+        new Duration($hours, $minutes, $seconds);
+
+    }
+
+    public function testSetDurationWithInvalidMinutesDataShouldThrowAnException()
+    {
+        $this->expectException(Exception::class);
+
+        $hours = '14';
+        $minutes = 'invalid minutes';
+        $seconds = '10';
+
+        new Duration($hours, $minutes, $seconds);
+    }
+
+    public function testSetDurationWithInvalidSecondsDataShouldThrowAnException()
+    {
+        $this->expectException(Exception::class);
+
+        $hours = '14';
+        $minutes = '25';
+        $seconds = 'invalid seconds';
+
+        new Duration($hours, $minutes, $seconds);
+    }
+
+    public function testToStringRegexShouldWork()
+    {
+        $hours = '14';
+        $minutes = '25';
+        $seconds = '10';
+
+        $duration = new Duration($hours, $minutes, $seconds);
+        $this->assertRegExp(
+          self::HOUR_REGULAR_EXPRESSION,
+          $duration->__toString()
+        );
+    }
+}
