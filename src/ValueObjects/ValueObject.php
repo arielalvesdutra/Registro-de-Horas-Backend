fdee699c6e93905770c18628ed1a86e408752e8e
arielalvesdutra <alves.lsn@gmail.com>
2019-02-16 20:14:02 -0200

    Adiciona o objeto de valor Duration para representar a duração do registro de tempo

---
+++ src/ValueObjects/ValueObject.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\ValueObjects;
+
+/**
+ * Objetos de valor não possuem métodos setters públicos, pois o valor dos atributos
+ * somente pode ser setado na criação do objetos.
+ *
+ * Class ValueObject
+ * @package App\ValueObjects
+ */
+abstract class ValueObject
+{
+}
