2646c5e862607baf3eab1ec5122dbbbd36c55113
arielalvesdutra <alves.lsn@gmail.com>
2019-02-16 20:15:25 -0200

    Atualiza testes unitários da App\Entities\TimeRecord

--- tests/Entities/TimeRecordTest.php
+++ tests/Entities/TimeRecordTest.php
@@ -2,10 +2,26 @@
 
 namespace App\Entities;
 
+use App\Decorators\DateTimeDecorator;
+use App\ValueObjects\Duration;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Testes unitários da entidade TimeRecord.
+ *
+ * Class TimeRecordTest
+ * @package App\Entities
+ */
 class TimeRecordTest extends TestCase
 {
+
+    const DATE_REGULAR_EXPRESSION = "/([0-9]{4})([\/])([0-9]{2})([\/])([0-9]{2})/";
+
+    const DATE_AND_HOUR_REGULAR_EXPRESSION =
+        '/([0-9]{4})([\/])([0-9]{2})([\/])([0-9]{2}) ([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})/';
+
+    const HOUR_REGULAR_EXPRESSION = "/([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})/";
+
     public function assertPreConditions(): void
     {
         $this->assertTrue(
@@ -14,24 +30,124 @@ class TimeRecordTest extends TestCase
         );
     }
 
-    public function testShouldGettersAndSettersWork()
+    public function testSetTitleWithEmptyDataValueShouldThrowAnException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $emptyString = '';
+        $timeRecord =  new TimeRecord();
+        $timeRecord->setTitle($emptyString);
+    }
+
+    public function testGetTitleShouldWork()
+    {
+        $title = "Registro de Tempo";
+        $timeRecord = new TimeRecord();
+        $timeRecord->setTitle($title);
+        $this->assertEquals($title, $timeRecord->getTitle());
+    }
+
+    public function testGetEndDateTimeShouldWork()
+    {
+        $endDateTime = new DateTimeDecorator("2019-01-01 10:00:00");
+        $timeRecord = new TimeRecord();
+        $timeRecord->setEndDateTime($endDateTime);
+
+        $this->assertEquals($endDateTime, $timeRecord->getEndDateTime());
+        $this->assertRegExp(
+            self::DATE_AND_HOUR_REGULAR_EXPRESSION,
+            $timeRecord->getEndDateTime()->__toString()
+        );
+    }
+
+    public function testSetEndDateTimeShouldWork()
+    {
+        $entDateTime = new DateTimeDecorator("2019/01/01 10:00:00");
+        $timeRecord = new TimeRecord();
+        $timeRecord->setEndDateTime($entDateTime);
+    }
+
+    public function testSetEndDateTimeWithInvalidDataShouldThrowAnException()
+    {
+        $this->expectException(\Throwable::class);
+        $timeRecord = new TimeRecord();
+        $timeRecord->setEndDateTime('Valor inválido.');
+    }
+
+    public function testEmptyGetEndDateTimeShouldThrowAnException()
+    {
+        $this->expectException(\Exception::class);
+        $timeRecord = new TimeRecord();
+        $timeRecord->getEndDateTime();
+    }
+
+    public function testGetInitDateTimeShouldWork()
+    {
+        $initDateTime = new DateTimeDecorator("2019-01-01 10:00:00");
+        $timeRecord = new TimeRecord();
+        $timeRecord->setInitDateTime($initDateTime);
+
+        $this->assertEquals($initDateTime, $timeRecord->getInitDateTime());
+        $this->assertRegExp(
+            self::DATE_AND_HOUR_REGULAR_EXPRESSION,
+            $timeRecord->getInitDateTime()->__toString()
+        );
+    }
+
+    public function testSetInitDateTimeShouldWork()
+    {
+        $initDateTime = new DateTimeDecorator("2019/01/01 10:00:00");
+        $timeRecord = new TimeRecord();
+        $timeRecord->setInitDateTime($initDateTime);
+    }
+
+    public function testSetInitDateTimeWithInvalidDataShouldThrowAnException()
+    {
+        $this->expectException(\Throwable::class);
+        $timeRecord = new TimeRecord();
+        $timeRecord->setInitDateTime('Valor inválido.');
+    }
+
+    public function testEmptyGetInitDateTimeShouldThrowAnException()
+    {
+        $this->expectException(\Exception::class);
+        $timeRecord = new TimeRecord();
+        $timeRecord->getInitDateTime();
+    }
+
+    public function testDurationGetterAndSetterShouldWork()
+    {
+        $hours = '14';
+        $minutes = '25';
+        $seconds = '10';
+
+        $duration = new Duration($hours, $minutes, $seconds);
+        $timeRecord = new TimeRecord();
+        $timeRecord->setDuration($duration);
+        $this->assertEquals($duration, $timeRecord->getDuration());
+        $this->assertRegExp(
+            self::HOUR_REGULAR_EXPRESSION,
+            $timeRecord->getDuration()->__toString()
+        );
+    }
+
+    public function testGettersAndSettersShouldWork()
     {
         $id = 1;
         $title = "Registro de tempo";
-        $initDate = "2019/01/01 10:00:00";
-        $endDate = "2019/01/01 12:00:00";
-        $duration = "2:00:00";
+        $initDateTime = new DateTimeDecorator("2019/01/01 10:00:00");
+        $endDateTime = new DateTimeDecorator("2019/01/01 12:00:00");
+        $duration = new Duration("2", "00", "00");
 
         $record = new TimeRecord(['id' => $id]);
         $record->setTitle($title)
-                ->setInitDate($initDate)
-                ->setEndDate($endDate)
+                ->setInitDateTime($initDateTime)
+                ->setEndDateTime($endDateTime)
                 ->setDuration($duration);
 
         $this->assertEquals($id, $record->getId(), 'Erro no set ou get do id.');
         $this->assertEquals($title, $record->getTitle(), 'Erro no set ou get do título.');
-        $this->assertEquals($initDate, $record->getInitDate(), 'Erro no set ou get da data inicial.');
-        $this->assertEquals($endDate, $record->getEndDate(), 'Erro no set ou get da data final.');
-        $this->assertEquals($duration, $record->getDuration(), 'Erro no set ou get da duração.');
+        $this->assertEquals($initDateTime->__toString(), $record->getInitDateTime()->__toString(), 'Erro no set ou get da data inicial.');
+        $this->assertEquals($endDateTime->__toString(), $record->getEndDateTime()->__toString(), 'Erro no set ou get da data final.');
+        $this->assertEquals($duration->__toString(), $record->getDuration()->__toString(), 'Erro no set ou get da duração.');
     }
 }
